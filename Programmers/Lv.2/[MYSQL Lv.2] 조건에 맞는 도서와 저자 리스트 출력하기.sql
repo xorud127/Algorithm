a5c2ce174dc574c00b3323cd8535ec9814d06e17
xorud127 <54922652+xorud127@users.noreply.github.com>
2025-04-21 22:43:06 +0900

    Create [MYSQL Lv.2] 조건에 맞는 도서와 저자 리스트 출력하기.sql

---
+++ Programmers/Lv.2/[MYSQL Lv.2] 조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,4 @@
+SELECT B.BOOK_ID, A.AUTHOR_NAME, date_format(B.PUBLISHED_DATE, "%Y-%m-%d") as PUBLISHED_DATE
+from BOOK B left join AUTHOR A on B.AUTHOR_ID = A.AUTHOR_ID
+where B.CATEGORY = "경제"
+order by B.PUBLISHED_DATE
