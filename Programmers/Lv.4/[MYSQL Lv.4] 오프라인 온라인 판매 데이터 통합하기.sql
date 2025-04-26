db1230f677cc89199befa3cd6b5eb95c24f66fb4
xorud127 <54922652+xorud127@users.noreply.github.com>
2025-04-26 18:39:15 +0900

    Create [MYSQL Lv.4] 오프라인 온라인 판매 데이터 통합하기.sql

---
+++ Programmers/Lv.4/[MYSQL Lv.4] 오프라인 온라인 판매 데이터 통합하기.sql
@@ -0,0 +1,8 @@
+(SELECT date_format(SALES_DATE, "%Y-%m-%d") as SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+from ONLINE_SALE
+where year(SALES_DATE) = 2022 and month(SALES_DATE) = 3
+union all
+select date_format(SALES_DATE, "%Y-%m-%d") as SALES_DATE, PRODUCT_ID, null as USER_ID, SALES_AMOUNT
+from OFFLINE_SALE
+where year(SALES_DATE) = 2022 and month(SALES_DATE) = 3)
+order by SALES_DATE, PRODUCT_ID, USER_ID
