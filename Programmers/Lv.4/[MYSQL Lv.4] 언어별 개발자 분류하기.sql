e594da845bf660bf265125430cbcf38d66637bb9
xorud127 <54922652+xorud127@users.noreply.github.com>
2025-04-27 18:55:20 +0900

    Create [MYSQL Lv.4] 언어별 개발자 분류하기.sql

---
+++ Programmers/Lv.4/[MYSQL Lv.4] 언어별 개발자 분류하기.sql
@@ -0,0 +1,21 @@
+with g as (
+    SELECT
+        CASE
+            WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End') > 0 
+                AND SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE NAME = 'Python') > 0
+            THEN 'A'
+            WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE NAME = 'C#') > 0
+            THEN 'B'
+            WHEN SKILL_CODE & (SELECT SUM(CODE) FROM SKILLCODES WHERE CATEGORY = 'Front End') > 0
+            THEN 'C'
+            ELSE NULL
+        END AS GRADE,
+        ID, EMAIL
+    FROM
+        DEVELOPERS
+    )
+
+SELECT *
+FROM g
+WHERE GRADE IS NOT NULL
+ORDER BY GRADE, ID
